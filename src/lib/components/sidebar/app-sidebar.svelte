a1e7d9dc4a3e0297b89e5b1a65f29eebef87c35c
Julien Roux <julien.roux404@gmail.com>
2026-05-18 22:01:28 +0200

    feat(sidebar): add sidebar components including navigation, user, and team switcher

---
+++ src/lib/components/sidebar/app-sidebar.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+	import AudioWaveformIcon from "@lucide/svelte/icons/audio-waveform";
+	import BookOpenIcon from "@lucide/svelte/icons/book-open";
+	import ChartPieIcon from "@lucide/svelte/icons/chart-pie";
+	import CommandIcon from "@lucide/svelte/icons/command";
+	import FrameIcon from "@lucide/svelte/icons/frame";
+	import GalleryVerticalEndIcon from "@lucide/svelte/icons/gallery-vertical-end";
+	import HouseIcon from "@lucide/svelte/icons/house";
+	import MapIcon from "@lucide/svelte/icons/map";
+	import Settings2Icon from "@lucide/svelte/icons/settings-2";
+	import TimelineIcon from "@lucide/svelte/icons/timeline";
+	import { page } from "$app/stores";
+	import NavMain from "./nav-main.svelte";
+	import NavProjects from "./nav-projects.svelte";
+	import NavUser from "./nav-user.svelte";
+	import TeamSwitcher from "./team-switcher.svelte";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import type { ComponentProps } from "svelte";
+
+	// This is sample data.
+	const data = {
+		user: {
+			name: "shadcn",
+			email: "m@example.com",
+			avatar: "/avatars/shadcn.jpg",
+		},
+		teams: [
+			{
+				name: "Acme Inc",
+				logo: GalleryVerticalEndIcon,
+				plan: "Enterprise",
+			},
+			{
+				name: "Acme Corp.",
+				logo: AudioWaveformIcon,
+				plan: "Startup",
+			},
+			{
+				name: "Evil Corp.",
+				logo: CommandIcon,
+				plan: "Free",
+			},
+		],
+		pages: [
+			{
+				title: "Dashboard",
+				url: "/",
+				icon: HouseIcon,
+			},
+			{
+				title: "Timeline",
+				url: "/timeline",
+				icon: TimelineIcon,
+			},
+			{
+				title: "Unused",
+				url: "#",
+				icon: BookOpenIcon,
+			},
+			{
+				title: "Settings",
+				url: "/settings",
+				icon: Settings2Icon,
+			},
+		],
+		projects: [
+			{
+				name: "Design Engineering",
+				url: "#",
+				icon: FrameIcon,
+			},
+			{
+				name: "Sales & Marketing",
+				url: "#",
+				icon: ChartPieIcon,
+			},
+			{
+				name: "Travel",
+				url: "#",
+				icon: MapIcon,
+			},
+		],
+	};
+
+	const pages = $derived(
+		data.pages.map((item) => ({
+			...item,
+			isActive: item.url !== "#" && $page.url.pathname === item.url,
+		}))
+	);
+
+	let {
+		ref = $bindable(null),
+		collapsible = "icon",
+		...restProps
+	}: ComponentProps<typeof Sidebar.Root> = $props();
+</script>
+
+<Sidebar.Root bind:ref {collapsible} {...restProps}>
+	<Sidebar.Header>
+		<TeamSwitcher teams={data.teams} />
+	</Sidebar.Header>
+	<Sidebar.Content>
+		<NavMain items={pages} />
+		<NavProjects projects={data.projects} />
+	</Sidebar.Content>
+	<Sidebar.Footer>
+		<NavUser user={data.user} />
+	</Sidebar.Footer>
+	<Sidebar.Rail />
+</Sidebar.Root>
