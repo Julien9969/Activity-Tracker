a159b9275672738f435c371c537f83520bbc524f
Julien Roux <julien.roux404@gmail.com>
2026-05-18 18:44:38 +0200

    chore: update dependencies and improve project structure
    - Added variable font sources for Inter and Roboto.
    - Updated @lucide/svelte to version 1.16.0.
    - Updated @tailwindcss/vite to version 4.3.0.
    - Updated layerchart to version 2.0.0-next.48.
    - Updated tailwind-merge to version 3.6.0.
    - Updated tailwindcss to version 4.3.0.
    - Added shadcn-svelte as a dependency.
    - Refactored app.css to use @import statements for better organization.
    - Removed redundant import of app.css in layout.svelte.
    - Updated layout.css to import app.css correctly.
    - Enhanced tsconfig.json with path aliases for $lib.
    - Updated vite.config.js to include path alias for $lib.

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ This template should help get you started developing with Tauri, SvelteKit and T
 [VS Code](https://code.visualstudio.com/) + [Svelte](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer).
 
 npx shadcn-svelte@latest add button
+
+Ideas :
+Snooze in context menu
+excluded app : regex on title, process exe and ?
+time notify
+assign color per apps (hashing exe name)
+timeline
+focus force
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import path from "path";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig(({
   plugins: [tailwindcss(), sveltekit()],
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
